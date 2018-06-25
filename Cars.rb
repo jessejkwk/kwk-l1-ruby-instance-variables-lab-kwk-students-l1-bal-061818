785189703b499cd740b99b8beb8d3e3d3c169eaa
jessejkwk <janderson@nyatoday.com>
2018-06-25 14:09:56 +0000

    automatically backed up by learn

--- Cars.rb
+++ Cars.rb
@@ -1,6 +1,6 @@
 class Cars
 
-  #for each instance, now you can change things within 
+  #for each instance, now you can change things within a template
   attr_accessor :stereo
 
   def initialize(model, make, year)
